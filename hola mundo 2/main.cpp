a244582202a5322e89c48f51163e1bf64cbac2bd
MadRam1 <24020839@uadeo.mx>
2024-09-23 20:19:23 -0700

    Add files via upload

---
+++ hola mundo 2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   printf("hola mundo");
+   printf("\n hola mundo");
+   printf("\n\n\n hola mundo \t\t\t");
+   printf("\n\n\n hola mundo \a");
+   printf("\n \"hola mundo\"");
+
+}
